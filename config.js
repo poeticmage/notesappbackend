023942254d77f02f5c3dbc8725a66f78e5f6042d
poeticmage <mookherjipriyadarshi@gmail.com>
2025-03-31 10:42:35 +0530

    Environments

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 
 
+import dotenv from "./.env";
 
+dotenv.config();
 
-export const tokenT="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";
-export const gst1="6ea841d08fb82c6ee876d34ce3adf812";
-export const gst2="76dc02015c9f796f2b24051eeb04f49d";
+export const tokenT=process.env.tokenT;
+export const gst1=process.env.gst1;
+export const gst2=process.env.gst2;
