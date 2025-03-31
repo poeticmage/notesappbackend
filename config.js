3d2690202ffd02aba0e32d4231950454fe879511
poeticmage <mookherjipriyadarshi@gmail.com>
2025-03-31 10:30:43 +0530

    First Commit

---
+++ config.js
@@ -0,0 +1,7 @@
+
+
+
+
+export const tokenT="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";
+export const gst1="6ea841d08fb82c6ee876d34ce3adf812";
+export const gst2="76dc02015c9f796f2b24051eeb04f49d";
---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import { Octokit } from "@octokit/core"; //git gist documentations
+import cors from "cors";//To handle different server request
+import { tokenT, gst1, gst2 } from "./config.js";
+import loginget from "./loginGet.js";
+import loginput from "./loginPut.js";
+
+//start
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());//For handling different server
+
+//Requests handling...
+app.use(loginget);
+app.use(loginput);
+
+app.listen(3000,()=>{
+    console.log("started");
+});
