ac953c5c0aa228466b8a43815d9a7dbbc24710dd
poeticmage <mookherjipriyadarshi@gmail.com>
2025-03-31 13:02:59 +0530

    again

--- loginPut.js
+++ loginPut.js
@@ -14,7 +14,7 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());//For handling different server
-const router=app.express();
+const router=express.Router();
 
 
 
